1a3445f9c6b1c844c9cfa000847a33d606f4f5fd
Lexie Lai <lexie160407@gmail.com>
2025-05-15 11:00:38 +0800

    2nd commit add groupAct

---
+++ src/main/java/com/dukak/groupact/model/GroupActivityVO.java
@@ -0,0 +1,14 @@
+package com.dukak.groupact.model;
+
+public class GroupActVO java.io.Serializable{
+	private String actname;
+	
+	public String getActname() {
+		return actname;
+	}
+	public void setActname(String actname) {
+		this.actname = actname;
+	}
+
+
+}
