386d415852f0dc14f0bee6d103a164ba03bbbecb
Lexie Lai <lexie160407@gmail.com>
2025-05-15 10:39:12 +0800

    first commit

---
+++ src/main/java/com/dukak/member/model/MemberVO.java
@@ -0,0 +1,23 @@
+package com.dukak.member.model;
+
+
+public class MemberVO implements java.io.Serializable{
+	private Integer memno;
+	private String ename;
+	
+	
+	public Integer getMemno() {
+		return memno;
+	}
+	public void setMemno(Integer memno) {
+		this.memno = memno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+
+}
+
